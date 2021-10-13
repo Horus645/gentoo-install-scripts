d2a63be215200cbca48d3d5919b542e2a0b8a0cc
horus645 <leonardo.fae44@gmail.com>
2021-10-13 09:25:20 -0300

    added eselect-repository

--- setup-make.conf.sh
+++ setup-make.conf.sh
@@ -12,7 +12,7 @@ grep -q "MAKEOPTS" "$CONF_FILE" || printf '\nMAKEOPTS="-j12"\n\n' >> "$CONF_FILE
 
 #vaapi -- ?
 grep -q "USE" "$CONF_FILE" || \
-	echo 'USE="alsa dbus elogind fmmpeg gtk lua_single_target_luajit '\
+	echo 'USE="alsa curl dbus elogind fmmpeg gtk lua_single_target_luajit '\
 	'opengl pulseaudio threads vim-syntax vaapi vdpau vulkan X wayland'\
 	'-bluetooth -cdr -cuda -dvd -emacs -gnome -kde -nvidia -jack'\
 	'-lua_single_target_lua5-1 -lua_single_target_lua5-3 -lua_single_target_lua5-4'\

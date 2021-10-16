139d9c80ba632c8ff02a42dfecdb005ccaafd33e
horus645 <leonardo.fae44@gmail.com>
2021-10-16 14:28:05 -0300

    Normalized file names. They all use '_' now.

--- setup-doas.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-[ "$(id -u)" -ne 0 ] && echo "Must run as root" && exit
-
-(echo "permit persist keepenv horus as root"
-echo "permit nopass horus cmd reboot"
-echo "permit nopass horus cmd poweroff") > /etc/doas.conf

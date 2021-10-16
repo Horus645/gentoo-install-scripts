e66a0859466e0c035cc1ac720c7cfe130d649917
horus645 <leonardo.fae44@gmail.com>
2021-10-15 21:13:05 -0300

    Now using a variable instead of repeating directory

--- install_packages_from_overlays.sh
+++ install_packages_from_overlays.sh
@@ -3,18 +3,20 @@
 # Make sure we are running as root:
 [ "$(id -u)" -ne 0 ] && echo "Must run as root" && exit
 
+ACCEPT_KEYWORDS_DIR="/etc/portage/package.accept_keywords"
+
 # Bellow, we always make sure to only unmask the packages we need
 
 # from the wayland overlay
 eselect repository enable wayland-desktop && emaint sync --repo wayland-desktop && \
-echo "gui-apps/yambar ~amd64" > /etc/portage/package.accept_keywords/wayland-desktop && \
+echo "gui-apps/yambar ~amd64" > "$ACCEPT_KEYWORDS_DIR"/wayland-desktop && \
 emerge --verbose gui-apps/yambar
 
 
 # from the guru overlay
 eselect repository enable guru && emaint sync --repo guru && \
-echo "gui-apps/foot ~amd64" > /etc/portage/package.accept_keywords/foot && \
-echo "dev-util/rust-analyzer ~amd64" > /etc/portage/package.accept_keywords/rust-analyzer && \
+echo "gui-apps/foot ~amd64" > "$ACCEPT_KEYWORDS_DIR"/foot && \
+echo "dev-util/rust-analyzer ~amd64" > "$ACCEPT_KEYWORDS_DIR"/rust-analyzer && \
 emerge --verbose gui-apps/foot dev-util/rust-analyzer
 
 # getting brave:
@@ -23,5 +25,7 @@ emerge --verbose www-client/brave-bin
 
 # getting hls:
 eselect repository enable haskell && emaint sync --repo haskell && \
-echo "dev-util/haskell-language-server ~amd64" > /etc/portage/package.accept_keywords/haskell-language-server && \
+echo "dev-util/haskell-language-server ~amd64" > "$ACCEPT_KEYWORDS_DIR"/haskell-language-server && \
 emerge --verbose dev-util/haskell-language-server
+
+# missing: steam
